f2ddda9d48e7afc50dd90227e2e2d4f7f0568181
Hielo Elemental <hieloelemental@gmail.com>
2024-06-03 10:45:33 -0500

    feat: :sparkles: lesson 6 - onClick and onChange
    add those two attributes to the input and button elements

--- src/TodoCreateButton.jsx
+++ src/TodoCreateButton.jsx
@@ -1,7 +1,9 @@
 import PlusIcon from './PlusIcon';
 
 const TodoCreateButton = ({children}) => {
-  return <button className='Icon Button'><span className='Icon PlusIcon'><PlusIcon /></span>{ children }</button>
+  return <button className='Icon Button' onClick={(event) => {
+    console.log("clicked")
+  }}><span className='Icon PlusIcon'><PlusIcon /></span>{ children }</button>
 }
 
 export { TodoCreateButton };
--- src/TodoSearchForm.jsx
+++ src/TodoSearchForm.jsx
@@ -2,7 +2,9 @@ const TodoSearchForm = () => {
   return (
     <div className="TodoForm">
       <label htmlFor="">Search:</label>
-      <input placeholder="Cut Onions" />
+      <input placeholder="Cut Onions" onChange={(event) => {
+        console.log(event.target.value);
+      }} />
     </div>
   )
 }
